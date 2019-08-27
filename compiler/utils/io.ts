a6367219482c55715f450ad0b5a5aad2b9ced5c1
Mark Skelton <mdskelton99@gmail.com>
2019-08-26 21:05:59 -0500

    Use writeJson command for all JSON writes

--- compiler/utils/io.ts
+++ compiler/utils/io.ts
@@ -1,6 +1,7 @@
-import { join } from 'path'
 import { promises as fs } from 'fs'
+import { join } from 'path'
 import { load as loadYaml } from 'yamljs'
+import { PRETTY_PRINT_JSON } from './env'
 
 export const INPUT_DIRECTORY = join(__dirname, '..', 'config')
 export const OUTPUT_DIRECTORY = join(__dirname, '..', '..', 'src', 'config')
@@ -18,7 +19,10 @@ export async function writeJson (path: string, data: any): Promise<void> {
   const directory = path.substring(0, path.lastIndexOf('/'))
   await fs.mkdir(directory, { recursive: true })
 
-  return fs.writeFile(path, JSON.stringify(data))
+  return fs.writeFile(
+    path,
+    JSON.stringify(data, null, PRETTY_PRINT_JSON ? 2 : 0)
+  )
 }
 
 export function listDirectory (path: string): Promise<string[]> {
--- compiler/write-scopes.ts
+++ compiler/write-scopes.ts
@@ -1,14 +1,9 @@
-import { promises as fs } from 'fs'
 import { join } from 'path'
 import { ScopeMap, Scopes } from './types'
-import { OUTPUT_DIRECTORY } from './utils/io'
-import { PRETTY_PRINT_JSON } from './utils/env'
+import { OUTPUT_DIRECTORY, writeJson } from './utils/io'
 
 const writeFile = (filename: string, scopeMap: ScopeMap): Promise<void> =>
-  fs.writeFile(
-    join(OUTPUT_DIRECTORY, `${filename}.json`),
-    JSON.stringify(scopeMap, null, PRETTY_PRINT_JSON ? 2 : 0)
-  )
+  writeJson(join(OUTPUT_DIRECTORY, `${filename}.json`), scopeMap)
 
 export async function writeScopes (scopes: Scopes): Promise<void> {
   await Promise.all([
