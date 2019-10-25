38f56f0a7cad1c33c55b3255685f72a2f4ee260a
Mark Skelton <mdskelton99@gmail.com>
2019-10-24 20:38:19 -0500

    Add models

---
+++ src/models/colors.ts
@@ -0,0 +1,12 @@
+export interface Colors {
+  chalky: string
+  coral: string
+  dark: string
+  error: string
+  fountainBlue: string
+  green: string
+  lightWhite: string
+  malibu: string
+  purple: string
+  whiskey: string
+}
