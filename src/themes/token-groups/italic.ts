156b0bcb70ef34639fe8efd3e665766f7eed8a15
Mark Skelton <mdskelton99@gmail.com>
2019-10-25 22:13:38 -0500

    Update tokens

--- src/themes/token-groups/italic.ts
+++ src/themes/token-groups/italic.ts
@@ -3,14 +3,30 @@ import { FontStyle, TokenGroup } from '../../models'
 export const italicTokens: TokenGroup[] = [
   {
     name: 'Italic',
-    scope: ['comment', 'keyword.control', 'storage.type', 'storage.modifier'],
+    scope: [
+      'comment',
+      'keyword.control',
+      'keyword.other',
+      'keyword.operator.new',
+      'storage.modifier',
+      'storage.type',
+      'variable.language.this',
+    ],
     settings: {
       fontStyle: FontStyle.Italics,
     },
   },
   {
     name: 'Reset',
-    scope: ['storage.type.function.arrow'],
+    scope: [
+      'keyword.other.type',
+      'storage.modifier.import',
+      'storage.type.built-in',
+      'storage.type.function.arrow',
+      'storage.type.generic',
+      'storage.type.java',
+      'storage.type.primitive',
+    ],
     settings: {
       fontStyle: FontStyle.None,
     },
--- src/themes/token-groups/standard.ts
+++ src/themes/token-groups/standard.ts
@@ -4,14 +4,36 @@ import { TokenGroup } from '../../models/tokens'
 export const getStandardTokens = (colors: Colors): TokenGroup[] => [
   {
     name: 'Chalky',
-    scope: ['entity.name.type', 'support.type'],
+    scope: [
+      'entity.name.type',
+      'entity.other.inherited-class',
+      'keyword.other.type',
+      'punctuation.definition.annotation',
+      'storage.modifier.import',
+      'storage.type.annotation',
+      'storage.type.built-in',
+      'storage.type.generic',
+      'storage.type.java',
+      'storage.type.primitive',
+      'support.class',
+      'support.other.namespace',
+      'support.type',
+      'variable.language.this',
+    ],
     settings: {
       foreground: colors.chalky,
     },
   },
   {
     name: 'Coral',
-    scope: ['entity.name.tag'],
+    scope: [
+      'entity.name.tag',
+      'meta.object-literal.key',
+      'punctuation.definition.template-expression',
+      'support.type.property-name',
+      'variable.object.property',
+      'variable.other.enummember',
+    ],
     settings: {
       foreground: colors.coral,
     },
@@ -24,15 +46,40 @@ export const getStandardTokens = (colors: Colors): TokenGroup[] => [
     },
   },
   {
+    name: 'Fountain Blue',
+    scope: ['keyword.operator'],
+    settings: {
+      foreground: colors.fountainBlue,
+    },
+  },
+  {
     name: 'Malibu',
-    scope: ['entity.name.function'],
+    scope: [
+      'entity.name.function',
+      'entity.other.attribute-name.id.css',
+      'support.function',
+    ],
     settings: {
       foreground: colors.malibu,
     },
   },
   {
+    name: 'Light white',
+    scope: ['punctuation.separator', 'support.type.property-name.css'],
+    settings: {
+      foreground: colors.lightWhite,
+    },
+  },
+  {
     name: 'Purple',
-    scope: ['keyword.control', 'storage.type', 'storage.modifier'],
+    scope: [
+      'keyword.control',
+      'keyword.operator.expression',
+      'keyword.operator.new',
+      'keyword.other',
+      'storage.modifier',
+      'storage.type',
+    ],
     settings: {
       foreground: colors.purple,
     },
@@ -47,9 +94,11 @@ export const getStandardTokens = (colors: Colors): TokenGroup[] => [
   {
     name: 'Whiskey',
     scope: [
-      'constant.language',
-      'variable.parameter',
+      'constant',
+      'entity.other.attribute-name.class.css',
+      'entity.other.attribute-name.pseudo-class.css',
       'variable.other.constant',
+      'variable.parameter',
     ],
     settings: {
       foreground: colors.whiskey,
