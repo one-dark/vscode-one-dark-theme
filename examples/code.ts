651dce356f194ead76c842af74f66121d2a6f7fe
mskelton <mskelton@users.noreply.github.com>
2020-04-12 02:49:23 +0000

    Apply formatting changes

--- examples/code.ts
+++ examples/code.ts
@@ -1,46 +1,47 @@
 module ModuleValidator {
-    import checkChars = CharUtils.notWhiteSpace;
-    export interface HasValidator<T> {
-        validateValue(): Boolean;
+  import checkChars = CharUtils.notWhiteSpace;
+  export interface HasValidator<T> {
+    validateValue(): Boolean;
+  }
+
+  type FooBarAlias = string;
+
+  @decorator()
+  class HasValidator implements HasValidator<String> {
+    /* Processed values */
+    static validatedValue: Array<String> = ["", "aa"];
+    private myValue: String;
+
+    /**
+     * Constructor for <code>HasValidator</code> class
+     * @param value for <i>validation</i>
+     */
+    constructor(valueParameter: String) {
+      this.myValue = valueParameter;
+      HasValidator.validatedValue.push(value);
     }
 
-    type FooBarAlias = string;
-
-    @decorator()
-    class HasValidator implements HasValidator<String> {
-        /* Processed values */
-        static validatedValue: Array<String> = ["", "aa"];
-        private myValue: String;
-
-        /**
-         * Constructor for <code>HasValidator</code> class
-         * @param value for <i>validation</i>
-         */
-        constructor(valueParameter: String) {
-            this.myValue = valueParameter;
-            HasValidator.validatedValue.push(value);
-        }
-
-        public validateValue(): Boolean {
-            var resultValue: Boolean = checkChars(this.myValue);
-            return resultValue;
-        }
-
-        static createInstance(valueParameter: string): HasValidator {
-            return new HasValidator(valueParameter);
-        }
+    public validateValue(): Boolean {
+      var resultValue: Boolean = checkChars(this.myValue);
+      return resultValue;
     }
 
-    function globalFunction<TypeParameter>(value: TypeParameter) { //global function
-        return 42;
+    static createInstance(valueParameter: string): HasValidator {
+      return new HasValidator(valueParameter);
     }
-    declare var declareUrl;
-    var varUrl = declareUrl.replace(/^\s*(.*)/, "$1").concat("\u1111\z\n");
-    var hello = () => console.log("hello");
-    HasValidator.createInstance(varUrl).validateValue();
-    function acceptsUnion(s: string | number) {
-        if (typeof s === "string") {
-            s
-        }
+  }
+
+  function globalFunction<TypeParameter>(value: TypeParameter) {
+    //global function
+    return 42;
+  }
+  declare var declareUrl;
+  var varUrl = declareUrl.replace(/^\s*(.*)/, "$1").concat("\u1111z\n");
+  var hello = () => console.log("hello");
+  HasValidator.createInstance(varUrl).validateValue();
+  function acceptsUnion(s: string | number) {
+    if (typeof s === "string") {
+      s;
     }
+  }
 }
