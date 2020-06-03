f850a4ec7345b2136d25dc99e0696c4096155b53
Mark Skelton <mdskelton99@gmail.com>
2020-06-03 08:49:39 -0500

    Add test boilerplate

---
+++ src/test/suite/index.ts
@@ -0,0 +1,35 @@
+import * as glob from "glob"
+import * as Mocha from "mocha"
+import * as path from "path"
+
+export function run(): Promise<void> {
+  const mocha = new Mocha({ ui: "tdd" })
+  mocha.useColors(true)
+
+  const testsRoot = path.resolve(__dirname, "..")
+
+  return new Promise((resolve, reject) => {
+    glob("**/**.spec.js", { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return reject(err)
+      }
+
+      // Add files to the test suite
+      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)))
+
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`))
+          } else {
+            resolve()
+          }
+        })
+      } catch (err) {
+        console.error(err)
+        reject(err)
+      }
+    })
+  })
+}
