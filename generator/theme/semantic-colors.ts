6a923f45713fd4e639a8eedbc0d2142a43e05e68
Mark Skelton <mdskelton99@gmail.com>
2020-08-17 07:58:33 -0500

    fix: Console color no longer yellow after VS Code 1.48 release

--- generator/theme/semantic-colors.ts
+++ generator/theme/semantic-colors.ts
@@ -4,5 +4,6 @@ export function getSemanticTokenColors(colors: Colors) {
   return {
     enumMember: colors.whiskey,
     property: colors.coral,
+    "variable.defaultLibrary": colors.chalky,
   }
 }
