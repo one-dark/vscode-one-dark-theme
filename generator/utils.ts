8f10d7446538c4609c3d7263c72827a16e9e4063
Mark Skelton <mdskelton99@gmail.com>
2021-03-05 16:40:12 -0600

    chore: Yarn 2 (#31)
    * Yarn 2
    
    * Fix peer deps
    
    * Update deps
    
    * CI
    
    * Fix type

--- generator/utils.ts
+++ generator/utils.ts
@@ -3,7 +3,7 @@ import * as fs from "fs"
 export async function writeFile(path: string, data: unknown) {
   return new Promise((resolve, reject) => {
     fs.writeFile(path, JSON.stringify(data), (err) =>
-      err ? reject(err) : resolve()
+      err ? reject(err) : resolve(null)
     )
   })
 }
